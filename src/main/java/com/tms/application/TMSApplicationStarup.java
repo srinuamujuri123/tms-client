d6536c2fa6dd84ed8677b0c8634195ae06281ad9
S.Sajid <shaiksajid492@gmail.com>
2023-03-29 23:31:42 +0530

    a

---
+++ src/main/java/com/tms/application/TMSApplicationStarup.java
@@ -0,0 +1,13 @@
+package com.tms.application;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+@EnableCaching
+public class TMSApplicationStarup {
+
+
+}
--- src/main/java/com/tms/client/model/TMSResponse.java
+++ src/main/java/com/tms/client/model/TMSResponse.java
@@ -3,6 +3,8 @@ package com.tms.client.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import lombok.Data;
+@Data
 @JsonInclude(Include.NON_NULL)
 public class TMSResponse {
 
@@ -14,46 +16,6 @@ public class TMSResponse {
 
 	public enum Status {
 		OK, FAILED
-	}
-
-	public Object getData() {
-		return data;
-	}
-
-	public void setData(Object data) {
-		this.data = data;
-	}
-
-	public Integer getCount() {
-		return count;
-	}
-
-	public void setCount(Integer count) {
-		this.count = count;
-	}
-
-	public Status getStatus() {
-		return status;
-	}
-
-	public void setStatus(Status status) {
-		this.status = status;
-	}
-
-	public String getDetails() {
-		return details;
-	}
-
-	public void setDetails(String details) {
-		this.details = details;
-	}
-
-	public String getErrorMessage() {
-		return errorMessage;
-	}
-
-	public void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
 	}
 
 }
